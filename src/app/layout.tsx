144339900b237c2e2ff4ef20d6b50baaf68d9ad9
Andres Bonilla <andresbonilla1256@gmail.com>
2023-07-13 12:00:46 -0500

    eliminacion del context no utilizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { SideBarContextProvider } from './context/SideBarContext'
 import { Navbar } from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,10 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SideBarContextProvider>
           <Navbar />
           {children}
-        </SideBarContextProvider>
       </body>
     </html>
   )
