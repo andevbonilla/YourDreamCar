959fa721f2d27e0fb853f011f6e7f124c75fa758
Andres Bonilla <andresbonilla1256@gmail.com>
2023-07-14 15:21:29 -0500

    agrege un coming soon temporal para online per..

--- src/app/personalize-yourdreamcar-online/page.tsx
+++ src/app/personalize-yourdreamcar-online/page.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Josefin_Sans } from '@next/font/google'
+const josefin = Josefin_Sans({ subsets: ['latin'], weight: '400' })
 
 const PersonalizeYourCarOnlinePage = () => {
   return (
-    <div>PersonalizeYourCarOnlinePage</div>
+    <main className={`${josefin.className} flex flex-col items-center md:px-32 bg-black text-white text-opacity-85 md:text-xl px-10 text-center h-full`}>
+      <div className='h-screen flex justify-center items-center'>
+        <h1 className='text-2xl font-bold'>Coming soon</h1>
+      </div> 
+    </main>
   )
 }
 
