c2b79eabbafb98af467614305739149104b04321
Andres Bonilla <andresbonilla1256@gmail.com>
2023-07-17 00:05:39 -0500

    nuevas imagenes agregadas para los servicios

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -34,19 +34,19 @@ const Services = () => {
            <div className='w-full cursor-pointer' onMouseEnter={() => showMoreInfo('Interior Wrap')} 
                                                   onMouseLeave={() => showMoreInfo('')} 
                                                   onClick={() => showMoreInfo('Interior Wrap')}>
-              <Service active={(whichActive === 'Interior Wrap')} name='Interior Wrap' desc={MinTextWrapService} img={'PPF-min.png'} />
+              <Service active={(whichActive === 'Interior Wrap')} name='Interior Wrap' desc={MinTextWrapService} img={'interior-wrap-tesla.png'} />
            </div>
 
            <div className='w-full cursor-pointer' onMouseEnter={() => showMoreInfo('Headlight & Taillight')} 
                                                   onMouseLeave={() => showMoreInfo('')} 
                                                   onClick={() => showMoreInfo('Headlight & Taillight')}>
-              <Service active={(whichActive === 'Headlight & Taillight')} name='Headlight & Taillight' desc={MinTextWrapService} img={'PPF-min.png'} />
+              <Service active={(whichActive === 'Headlight & Taillight')} name='Headlight & Taillight' desc={MinTextWrapService} img={'headlight-taillight.png'} />
            </div>
 
            <div className='w-full cursor-pointer' onMouseEnter={() => showMoreInfo('Caliper Paint')} 
                                                   onMouseLeave={() => showMoreInfo('')} 
                                                   onClick={() => showMoreInfo('Caliper Paint')}>
-              <Service active={(whichActive === 'Caliper Paint')} name='Caliper Paint' desc={MinTextWrapService} img={'PPF-min.png'} />
+              <Service active={(whichActive === 'Caliper Paint')} name='Caliper Paint' desc={MinTextWrapService} img={'caliper-blue.png'} />
            </div>
            
     </div>
