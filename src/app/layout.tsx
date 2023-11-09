abc3d833e7d52d6004a1dad67be306cc08bfc9cf
Andres Bonilla <andresbonilla1256@gmail.com>
2023-11-09 17:04:37 -0500

    stadistics and CEO improves

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import { Navbar } from '@/components/Navbar'
-import { Footer } from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'YourDreamCar',
   description: "YourDreamCar is a company based in Canada. We do wrapping, window tinting and more. Do not hesitate and create the car of your dreams.",
-  keywords: "Car Wrap,Car Wrap Toronto,Colour Change,Window Tinting, Tesla car Wrap"
+  keywords: ["Car Wrap","Car Wrap Toronto", "Colour Change", "Window Tinting", "Tesla car Wrap"]
 }
 
 export default function RootLayout({
@@ -23,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
           <Navbar />
           {children}
+          <Analytics />
           {/* footer section */}
           <footer className='md:px-32 md:pb-16 flex flex-col px-8 pt-12 pb-12 bg-black text-white'>
             <h2 className={`text-center font-bold text-2xl pb-12 underline decoration-[#18debe] underline-offset-8`}>SOCIAL MEDIA</h2>
